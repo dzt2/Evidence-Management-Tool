ab8630a735c65d903b1423797dbd1be64c777ed4
dzt <dzt@192.168.102.254>
2014-08-24 10:43:22 +0800

    update

--- Predicate Inferencer/src/cn/edu/buaa/sei/emt/logic/driver/Test.java
+++ Predicate Inferencer/src/cn/edu/buaa/sei/emt/logic/driver/Test.java
@@ -13,7 +13,14 @@ import cn.edu.buaa.sei.emt.logic.predicate.core.LogicFormulationTypeLoader;
 import cn.edu.buaa.sei.lmf.LMFContext;
 
 public class Test {
-	public static void main(String[] args){
+	public static void main(String[] args) throws Exception{
+		if(args.length!=4){
+			throw new Exception("Formation Errors: IM def_file assign_file target_name");
+		}
+		
+		if(!args[0].equals("IM"))
+			throw new Exception("Command Name must be \"IM\"");
+		
 		LMFContext.load(new LogicFormulationTypeLoader());
 		LMFContext.pack();
 		
@@ -27,10 +34,10 @@ public class Test {
 		machine.configValueInterpreter(new TextualValueInterpreter("VINTERPRETER_I"));
 		
 		try {
-			machine.configDefinitionInput(new FileInputStream("test.txt"));
-			machine.configAssignmentInput(new FileInputStream("assign.txt"));
+			machine.configDefinitionInput(new FileInputStream(args[1]));
+			machine.configAssignmentInput(new FileInputStream(args[2]));
 			
-			String id = "P";
+			String id = args[3];
 			Boolean res = machine.compute(id);
 			System.out.println("Inference Result: "+res);
 		} catch (FileNotFoundException e) {
