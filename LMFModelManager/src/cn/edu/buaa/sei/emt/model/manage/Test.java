ff6ad281ff20adccbc72a928fb69b3e3d19a8511
dzt <dzt@192.168.102.254>
2014-08-26 16:09:24 +0800

    creating model managed for LMF

---
+++ LMFModelManager/src/cn/edu/buaa/sei/emt/model/manage/Test.java
@@ -0,0 +1,15 @@
+package cn.edu.buaa.sei.emt.model.manage;
+
+import cn.edu.buaa.sei.lmf.LMFContext;
+import cn.edu.buaa.sei.lmf.Type;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LMFContext.pack();
+		
+		Type t = new Type();
+	}
+
+}
