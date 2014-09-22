455c2f9a1b943cb7d24366ddd5a71b832133179f
dzt <dzt@192.168.102.254>
2014-09-22 22:30:30 +0800

    11123

--- LogicAnalyzer_Computer/src/cn/edu/buaa/sei/logicAC/meta/logic/LogicExpression.java
+++ LogicAnalyzer_Computer/src/cn/edu/buaa/sei/logicAC/meta/logic/LogicExpression.java
@@ -3,6 +3,4 @@ package cn.edu.buaa.sei.logicAC.meta.logic;
 import cn.edu.buaa.sei.logicAC.meta.common.Expression;
 
 public interface LogicExpression extends LogicFormulation, Expression{
-	public LogicOperator getOperator();
-	public LogicFormulation getParameter(int i);
 }
