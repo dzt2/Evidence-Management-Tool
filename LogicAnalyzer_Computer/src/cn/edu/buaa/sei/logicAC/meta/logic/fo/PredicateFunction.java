df161ba9b473c2c7087e037e2692feaf2d3ca450
dzt <dzt@192.168.102.254>
2014-09-24 15:12:08 +0800

    rc5

--- LogicAnalyzer_Computer/src/cn/edu/buaa/sei/logicAC/meta/logic/fo/PredicateFunction.java
+++ LogicAnalyzer_Computer/src/cn/edu/buaa/sei/logicAC/meta/logic/fo/PredicateFunction.java
@@ -1,8 +1,8 @@
 package cn.edu.buaa.sei.logicAC.meta.logic.fo;
 
 import cn.edu.buaa.sei.logicAC.meta.common.function.ContextDependFunction;
+import cn.edu.buaa.sei.logicAC.meta.logic.common.LogicFunction;
 
-public interface PredicateFunction extends ContextDependFunction{
-	public void setTemplate(PredicateTemplate template);
+public interface PredicateFunction extends LogicFunction,ContextDependFunction{
 	public PredicateTemplate getTemplate();
 }
