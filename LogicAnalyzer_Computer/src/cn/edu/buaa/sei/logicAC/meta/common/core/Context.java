d1aa045fce6d9e43b2a95db31e822fc05f004b93
dzt <dzt@192.168.102.254>
2014-09-24 17:06:30 +0800

    33

--- LogicAnalyzer_Computer/src/cn/edu/buaa/sei/logicAC/meta/common/core/Context.java
+++ LogicAnalyzer_Computer/src/cn/edu/buaa/sei/logicAC/meta/common/core/Context.java
@@ -1,8 +1,31 @@
 package cn.edu.buaa.sei.logicAC.meta.common.core;
 
+/**
+ * Context provide environment information for executing the computation of target computable.
+ * */
 public interface Context {
+	/**
+	 * Return whether a computable is set in the context.
+	 * @exception {null} - unit is null.
+	 * */
 	public boolean containComputableUnit(ContextDependComputable unit);
+	/**
+	 * Add the context to a new target computable.<br>
+	 * 1) The context of the target computable is this one --> Do nothing.<br>
+	 * 2) The context of the target computable is not null --> Remove from the original context at first. <br>
+	 * 3) The target computable is null --> exception.
+	 * @exception Exception -- Null unit 
+	 * */
 	public void addComputableUnit(ContextDependComputable unit) throws Exception;
+	/**
+	 * Remove the context from the target computable.<br>
+	 * 1) The context of the target computable is this one --> just remove and set its context null.<br>
+	 * 2) The context of the target computable is not this one --> exception
+	 * 3) The target computable is null. --> exception
+	 * */
 	public void removeComputableUnit(ContextDependComputable unit) throws Exception;
+	/**
+	 * Return all the computable in the context.
+	 * */
 	public Computable[] getComputableUnits();
 }
