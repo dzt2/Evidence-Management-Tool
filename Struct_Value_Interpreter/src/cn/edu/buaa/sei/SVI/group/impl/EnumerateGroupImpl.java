e2a91917c8b51ae9707253741bba9b6cd2936cdf
dzt <dzt@192.168.102.254>
2014-10-14 17:33:34 +0800

    group impl

---
+++ Struct_Value_Interpreter/src/cn/edu/buaa/sei/SVI/group/impl/EnumerateGroupImpl.java
@@ -0,0 +1,35 @@
+package cn.edu.buaa.sei.SVI.group.impl;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import cn.edu.buaa.sei.SVI.group.EnumerateGroup;
+
+public class EnumerateGroupImpl implements EnumerateGroup{
+	Set<Object> set = new HashSet<Object>();
+
+	public EnumerateGroupImpl(){}
+	
+	@Override
+	public int size() {return this.set.size();}
+	@Override
+	public boolean contains(Object val) {return this.set.contains(val);}
+	
+	@Override
+	public void add(Object obj) {this.set.add(obj);}
+	@Override
+	public void remove(Object obj) {
+		if(this.set.contains(obj))
+			this.set.remove(obj);
+	}
+	@Override
+	public void addAll(EnumerateGroup grp) {
+		Iterator<Object> itor = grp.iterator();
+		while(itor.hasNext())
+			this.set.add(itor.next());
+	}
+	@Override
+	public Iterator<Object> iterator() {return this.set.iterator();}
+
+}
