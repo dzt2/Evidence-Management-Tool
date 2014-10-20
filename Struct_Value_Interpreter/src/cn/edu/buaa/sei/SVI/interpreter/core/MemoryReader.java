12be1f19ce440a494e551586391d33b04b6a8e5a
dzt <dzt@192.168.102.254>
2014-10-20 13:35:46 +0800

    change 001.001

--- Struct_Value_Interpreter/src/cn/edu/buaa/sei/SVI/interpreter/core/MemoryReader.java
+++ Struct_Value_Interpreter/src/cn/edu/buaa/sei/SVI/interpreter/core/MemoryReader.java
@@ -2,6 +2,6 @@ package cn.edu.buaa.sei.SVI.interpreter.core;
 
 import cn.edu.buaa.sei.SVI.struct.core.variable.Bindable;
 
-public interface MemoryReader{
+public interface MemoryReader extends Interpreter{
 	public Object interpret(Bindable var) throws Exception;
 }
--- Struct_Value_Interpreter/src/cn/edu/buaa/sei/SVI/interpreter/core/impl/MemoryReaderImpl.java
+++ Struct_Value_Interpreter/src/cn/edu/buaa/sei/SVI/interpreter/core/impl/MemoryReaderImpl.java
@@ -1,6 +1,7 @@
 package cn.edu.buaa.sei.SVI.interpreter.core.impl;
 
 import cn.edu.buaa.sei.SVI.interpreter.core.MemoryReader;
+import cn.edu.buaa.sei.SVI.struct.core.Struct;
 import cn.edu.buaa.sei.SVI.struct.core.variable.Bindable;
 
 public class MemoryReaderImpl implements MemoryReader{
@@ -10,5 +11,13 @@ public class MemoryReaderImpl implements MemoryReader{
 		if(var==null)
 			throw new Exception("Reading unknown variable");
 		return var.read();
+	}
+
+	@Override
+	public Object interpret(Struct input) throws Exception {
+		if(input==null)throw new Exception("Null input is invalid");
+		if(!(input instanceof Bindable))throw new Exception("Bindable required");
+		Bindable x = (Bindable) input;
+		return this.interpret(x);
 	}
 }
